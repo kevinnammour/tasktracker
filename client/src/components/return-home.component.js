6295e483474935390b31b1fe4dd9d42759af4381
kevinnammour <nammourkevin@gmail.com>
2022-02-12 12:23:39 +0200

    fixed loc

--- client/src/components/return-home.component.js
+++ client/src/components/return-home.component.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import "../styles/return_home_button.css";
 const ReturnHome = () => {
-  let history = useHistory();
   return (
     <>
-      <button onClick={() => history.goBack()}>Back</button>
+      <button onClick={() => window.location.href = '/'}>Back</button>
     </>
   );
 };
